1c80e24fd1429d710e067f714ea74d3a49a5de37
Deylan Shiva <18DDT19F1066@student.ptss.edu.my>
2021-04-20 11:57:53 +0800

    Add project files.

---
+++ Function Dan Sub/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnBaris1 = New System.Windows.Forms.Button()
+        Me.lblBaris1 = New System.Windows.Forms.Label()
+        Me.txtBaris2 = New System.Windows.Forms.TextBox()
+        Me.btnBaris2 = New System.Windows.Forms.Button()
+        Me.lblBaris2 = New System.Windows.Forms.Label()
+        Me.lblBaris3 = New System.Windows.Forms.Label()
+        Me.btnBaris3 = New System.Windows.Forms.Button()
+        Me.txtBaris4 = New System.Windows.Forms.TextBox()
+        Me.btnBaris4 = New System.Windows.Forms.Button()
+        Me.lblBaris4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnBaris1
+        '
+        Me.btnBaris1.Location = New System.Drawing.Point(292, 109)
+        Me.btnBaris1.Name = "btnBaris1"
+        Me.btnBaris1.Size = New System.Drawing.Size(75, 23)
+        Me.btnBaris1.TabIndex = 0
+        Me.btnBaris1.Text = "Button1"
+        Me.btnBaris1.UseVisualStyleBackColor = True
+        '
+        'lblBaris1
+        '
+        Me.lblBaris1.AutoSize = True
+        Me.lblBaris1.Location = New System.Drawing.Point(445, 114)
+        Me.lblBaris1.Name = "lblBaris1"
+        Me.lblBaris1.Size = New System.Drawing.Size(39, 13)
+        Me.lblBaris1.TabIndex = 1
+        Me.lblBaris1.Text = "Label1"
+        '
+        'txtBaris2
+        '
+        Me.txtBaris2.Location = New System.Drawing.Point(292, 182)
+        Me.txtBaris2.Name = "txtBaris2"
+        Me.txtBaris2.Size = New System.Drawing.Size(100, 20)
+        Me.txtBaris2.TabIndex = 2
+        '
+        'btnBaris2
+        '
+        Me.btnBaris2.Location = New System.Drawing.Point(434, 182)
+        Me.btnBaris2.Name = "btnBaris2"
+        Me.btnBaris2.Size = New System.Drawing.Size(75, 23)
+        Me.btnBaris2.TabIndex = 3
+        Me.btnBaris2.Text = "Button2"
+        Me.btnBaris2.UseVisualStyleBackColor = True
+        '
+        'lblBaris2
+        '
+        Me.lblBaris2.AutoSize = True
+        Me.lblBaris2.Location = New System.Drawing.Point(547, 187)
+        Me.lblBaris2.Name = "lblBaris2"
+        Me.lblBaris2.Size = New System.Drawing.Size(39, 13)
+        Me.lblBaris2.TabIndex = 4
+        Me.lblBaris2.Text = "Label2"
+        '
+        'lblBaris3
+        '
+        Me.lblBaris3.AutoSize = True
+        Me.lblBaris3.Location = New System.Drawing.Point(445, 268)
+        Me.lblBaris3.Name = "lblBaris3"
+        Me.lblBaris3.Size = New System.Drawing.Size(39, 13)
+        Me.lblBaris3.TabIndex = 5
+        Me.lblBaris3.Text = "Label3"
+        '
+        'btnBaris3
+        '
+        Me.btnBaris3.Location = New System.Drawing.Point(292, 263)
+        Me.btnBaris3.Name = "btnBaris3"
+        Me.btnBaris3.Size = New System.Drawing.Size(75, 23)
+        Me.btnBaris3.TabIndex = 6
+        Me.btnBaris3.Text = "Button3"
+        Me.btnBaris3.UseVisualStyleBackColor = True
+        '
+        'txtBaris4
+        '
+        Me.txtBaris4.Location = New System.Drawing.Point(292, 348)
+        Me.txtBaris4.Name = "txtBaris4"
+        Me.txtBaris4.Size = New System.Drawing.Size(100, 20)
+        Me.txtBaris4.TabIndex = 7
+        '
+        'btnBaris4
+        '
+        Me.btnBaris4.Location = New System.Drawing.Point(434, 348)
+        Me.btnBaris4.Name = "btnBaris4"
+        Me.btnBaris4.Size = New System.Drawing.Size(75, 23)
+        Me.btnBaris4.TabIndex = 8
+        Me.btnBaris4.Text = "Button4"
+        Me.btnBaris4.UseVisualStyleBackColor = True
+        '
+        'lblBaris4
+        '
+        Me.lblBaris4.AutoSize = True
+        Me.lblBaris4.Location = New System.Drawing.Point(547, 353)
+        Me.lblBaris4.Name = "lblBaris4"
+        Me.lblBaris4.Size = New System.Drawing.Size(39, 13)
+        Me.lblBaris4.TabIndex = 9
+        Me.lblBaris4.Text = "Label4"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblBaris4)
+        Me.Controls.Add(Me.btnBaris4)
+        Me.Controls.Add(Me.txtBaris4)
+        Me.Controls.Add(Me.btnBaris3)
+        Me.Controls.Add(Me.lblBaris3)
+        Me.Controls.Add(Me.lblBaris2)
+        Me.Controls.Add(Me.btnBaris2)
+        Me.Controls.Add(Me.txtBaris2)
+        Me.Controls.Add(Me.lblBaris1)
+        Me.Controls.Add(Me.btnBaris1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnBaris1 As Button
+    Friend WithEvents lblBaris1 As Label
+    Friend WithEvents txtBaris2 As TextBox
+    Friend WithEvents btnBaris2 As Button
+    Friend WithEvents lblBaris2 As Label
+    Friend WithEvents lblBaris3 As Label
+    Friend WithEvents btnBaris3 As Button
+    Friend WithEvents txtBaris4 As TextBox
+    Friend WithEvents btnBaris4 As Button
+    Friend WithEvents lblBaris4 As Label
+End Class
---
+++ Function Dan Sub/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Function Dan Sub")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Function Dan Sub")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("fd3b5225-80e4-46ff-b438-d6203e703356")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
